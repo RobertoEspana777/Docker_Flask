eefde8a1e65668464080fdeb69920f111616cb8c
RobertoEspana777 <132516678+RobertoEspana777@users.noreply.github.com>
2023-06-30 04:36:01 -0400

    Update Docker_Flask.sh

--- Docker_Flask.sh
+++ Docker_Flask.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Verificar si el usuario tiene Docker instalado
+if ! command -v docker &> /dev/null; then
+    echo "Error: Docker no está instalado. Por favor, instala Docker antes de continuar."
+    exit 1
+fi
+
+# Descargar la imagen desde Docker Hub
 docker pull robertoorfeo22/flaskdocker1
+
+# Ejecutar el contenedor con la imagen descargada
 docker run -d -p 1018:1018 --name flaskdocker1 robertoorfeo22/flaskdocker1
+
+# Mostrar los registros del contenedor en tiempo real
 docker logs -f flaskdocker1
